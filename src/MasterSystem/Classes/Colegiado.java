6e79554f9980a5bed6a7d125049987db7f773b5b
x2osso <30390740+x2osso@users.noreply.github.com>
2020-06-23 23:55:44 -0300

    Update Colegiado.java

--- src/MasterSystem/Classes/Colegiado.java
+++ src/MasterSystem/Classes/Colegiado.java
@@ -1,5 +1,24 @@
 package MasterSystem.Classes;
 
-public class Colegiado {
+import java.util.ArrayList;
 
+public class Colegiado {
+	Curso curso;
+	public static ArrayList<Professor> professores = new ArrayList<Professor>();
+	
+	public Colegiado() {
+		
+	}
+	public static void setProfessores(ArrayList<Professor> professores) {
+		Colegiado.professores = professores;
+	}
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+	public static ArrayList<Professor> getProfessores() {
+		return professores;
+	}
+	public Curso getCurso() {
+		return curso;
+	}
 }
